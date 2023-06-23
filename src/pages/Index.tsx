8745dab3efaf3d7caddfbfab4a86d40f2d1eb6df
Harry Permadi <harrypermaddy@gmail.com>
2023-06-23 15:16:37 +0700

    feat: get user from github api

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,9 +16,53 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import StarIcon from "@mui/icons-material/Star";
+import { Octokit } from "octokit";
+
+interface UserDetails {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: any;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  score: number;
+  site_admin: boolean;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
+}
 
 function SearchUser() {
+  const [users, setUsers] = React.useState<UserDetails[] | []>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const octokit = new Octokit({
+    auth: process.env.REACT_APP_GITHUB_TOKEN,
+  });
+
+  const getUsers = async () => {
+    setUsers([]);
+
+    try {
+      let result = await octokit.request("GET /search/users", {
+        q: searchTerm,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
+      setUsers(result.data.items);
+      console.log(result.data.items);
+    } catch (error: any) {
+      console.log(error.response);
+    }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -27,8 +71,8 @@ function SearchUser() {
   const submitForm = (event: React.FormEvent<HTMLElement>) => {
     event.preventDefault();
 
-    alert(searchTerm)
-  }
+    getUsers();
+  };
 
   return (
     <>
@@ -53,12 +97,13 @@ function SearchUser() {
               {searchTerm && (
                 <Typography>Showing users for "{searchTerm}"</Typography>
               )}
+              {users?.map((item) => (
               <Accordion elevation={0} disableGutters>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
                   sx={{ backgroundColor: "#CCCCCC" }}
                 >
-                  <Typography>Exampleuser 1</Typography>
+                  <Typography>{item.login}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                   <Card variant="outlined" sx={{ marginLeft: 2 }}>
@@ -83,6 +128,7 @@ function SearchUser() {
                   </Card>
                 </AccordionDetails>
               </Accordion>
+              ))}
             </Stack>
           </form>
         </Box>
