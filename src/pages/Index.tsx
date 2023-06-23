2f2c0e151f91774f9103877899866e9ac279dc16
Harry Permadi <harrypermaddy@gmail.com>
2023-06-23 13:25:28 +0700

    feat: submit form handler

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,39 +1,33 @@
-import React, { useRef, useState } from 'react';
-import { CssBaseline, Container, Box, Button, Stack, Accordion, AccordionSummary, Typography, AccordionDetails, Card, CardContent, FormControl, OutlinedInput } from "@mui/material"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import StarIcon from "@mui/icons-material/Star"
-
-interface UserDetails {
-  title: string;
-  description: string;
-}
+import React, { useState } from "react";
+import {
+  CssBaseline,
+  Container,
+  Box,
+  Button,
+  Stack,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
+  Card,
+  CardContent,
+  FormControl,
+  OutlinedInput,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import StarIcon from "@mui/icons-material/Star";
 
 function SearchUser() {
-  const [users, setUsers] = useState<UserDetails[]>([])
-  const timeout = useRef<any>()
-  const inputRef = useRef<any>()
-
-  const getUsers = () => {
-    clearTimeout(timeout.current)
-
-    if (!inputRef?.current?.value?.trim()) {
-      setUsers([])
-      return
-    }
-
-    timeout.current = setTimeout(async () => {
-      setUsers([])
-
-      // try {
-      //   setUsers([])
-      // } catch (error) {
-      //   setUsers([])
-      // }
-    }, 500)
-  }
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    getUsers()
+    setSearchTerm(event.target.value);
+  };
+
+  const submitForm = (event: React.FormEvent<HTMLElement>) => {
+    event.preventDefault();
+
+    alert(searchTerm)
   }
 
   return (
@@ -41,38 +35,56 @@ function SearchUser() {
       <CssBaseline />
       <Container maxWidth="sm">
         <Box alignContent="center" mt={4}>
-          <Stack direction="column" spacing={2}>
-            <FormControl fullWidth>
-              <OutlinedInput 
-                size="small" 
-                placeholder="Enter username"
-                onChange={handleChange}
-                inputRef={inputRef}
-                autoComplete="off"
-              />
-            </FormControl>
-            <Button variant="contained" fullWidth>Search</Button>
-            <Typography>Showing users for "Exampleuser"</Typography>
-            <Accordion elevation={0} disableGutters>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ backgroundColor: "#CCCCCC"}}>
-                <Typography>Exampleuser 1</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Card variant="outlined" sx={{ marginLeft: 2 }}>
-                  <CardContent>
-                    <Box display="flex" justifyContent="space-between">
-                      <Typography variant="h6" fontWeight={700}>Repository title</Typography>
-                      <Box display="flex" flexDirection="row" alignItems="center">
-                        <Typography sx={{ paddingRight: 1, fontWeight: 700}}>12</Typography>
-                        <StarIcon />
+          <form onSubmit={submitForm}>
+            <Stack direction="column" spacing={2}>
+              <FormControl fullWidth>
+                <OutlinedInput
+                  name="user"
+                  size="small"
+                  placeholder="Enter username"
+                  value={searchTerm}
+                  onChange={handleChange}
+                  autoComplete="off"
+                />
+              </FormControl>
+              <Button type="submit" variant="contained" fullWidth>
+                Search
+              </Button>
+              {searchTerm && (
+                <Typography>Showing users for "{searchTerm}"</Typography>
+              )}
+              <Accordion elevation={0} disableGutters>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  sx={{ backgroundColor: "#CCCCCC" }}
+                >
+                  <Typography>Exampleuser 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Card variant="outlined" sx={{ marginLeft: 2 }}>
+                    <CardContent>
+                      <Box display="flex" justifyContent="space-between">
+                        <Typography variant="h6" fontWeight={700}>
+                          Repository title
+                        </Typography>
+                        <Box
+                          display="flex"
+                          flexDirection="row"
+                          alignItems="center"
+                        >
+                          <Typography sx={{ paddingRight: 1, fontWeight: 700 }}>
+                            12
+                          </Typography>
+                          <StarIcon />
+                        </Box>
                       </Box>
-                    </Box>
-                    <Typography>Repository description</Typography>
-                  </CardContent>
-                </Card>
-              </AccordionDetails>
-            </Accordion>
-          </Stack>
+                      <Typography>Repository description</Typography>
+                    </CardContent>
+                  </Card>
+                </AccordionDetails>
+              </Accordion>
+            </Stack>
+          </form>
         </Box>
       </Container>
     </>
